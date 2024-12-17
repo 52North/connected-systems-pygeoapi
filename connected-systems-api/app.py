232d3926a836c564fad1485950046d75ed5fa0d6
Henning Bredel <881756+ridoo@users.noreply.github.com>
2024-12-17 16:09:30 +0100

    Move route config to top

--- connected-systems-api/app.py
+++ connected-systems-api/app.py
@@ -46,6 +46,13 @@ APP = cors(APP)
 APP.url_map.strict_slashes = False
 APP.config['JSONIFY_PRETTYPRINT_REGULAR'] = CONFIG['server'].get('pretty_print', False)
 
+APP.register_blueprint(csa)
+APP.register_blueprint(edr)
+APP.register_blueprint(stac)
+APP.register_blueprint(oapip)
+APP.register_blueprint(coverage)
+APP.register_blueprint(collections)
+
 if os.getenv("QUART_AUTH_BASIC", True):
 
     if (os.getenv("QUART_AUTH_BASIC_USERNAME") is None or os.getenv("QUART_AUTH_BASIC_PASSWORD") is None):
@@ -69,16 +76,6 @@ if os.getenv("QUART_AUTH_BASIC", True):
     async def is_auth():
         # Auth is handled by @basic_auth_required wrapper already
         return None
-
-  APP.register_blueprint(csa)
-
-  # TODO: make this configurable, only import required/configured
-  APP.register_blueprint(edr)
-  APP.register_blueprint(stac)
-  APP.register_blueprint(oapip)
-  APP.register_blueprint(coverage)
-  APP.register_blueprint(collections)
-
 
 @APP.get('/')
 async def landing_page():

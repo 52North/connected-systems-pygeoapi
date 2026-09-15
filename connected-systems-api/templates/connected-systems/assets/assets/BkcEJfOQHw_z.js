bfda832179d8a83ca910841f1a4218bd6e5bcdd5
Arne Vogt <a.vogt@52north.org>
2026-09-15 17:18:08 +0200

    update viewer

---
+++ connected-systems-api/templates/connected-systems/assets/assets/BkcEJfOQHw_z.js
@@ -0,0 +1,1 @@
+var e=JSON.parse(`{"connected-systems-viewer":{},"@open-pioneer/chakra-snippets":{"clipboard.copy":"copy","clipboard.copied":"copied","close-button.ariaLabel":"Close","carousel.next":"Next","carousel.prev":"Previous","password-input.triggerAriaLabel":"Toggle password visibility"}}`);export{e as default};
